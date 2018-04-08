1ab0e2d2eb6ed7fc424443fcb3208fed5d1ced8e
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-04-08 11:29:59 +0900

    Add project files.

---
+++ HttpClientMockSpike/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HttpClientMockSpike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
